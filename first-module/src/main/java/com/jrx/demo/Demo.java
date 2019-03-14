1add9f87a01e79e501ace7a990eda65d71a5ff6c
scy <120@qq.com>
2019-03-14 15:31:05 +0800

    第一次提交

---
+++ first-module/src/main/java/com/jrx/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.jrx.demo;
+
+/**
+ * @Author: sunchuanyin
+ * @Date: 2019/3/14 15:28
+ * @Version 1.0
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
