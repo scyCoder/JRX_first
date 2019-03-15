ab03c918921cf711f16f3ae883a5475832a33f34
scy <120@qq.com>
2019-03-15 09:44:02 +0800

    第一次提交

--- first-module/src/main/java/com/jrx/demo/Demo.java
+++ first-module/src/main/java/com/jrx/demo/Demo.java
@@ -1,7 +1,5 @@
 package com.jrx.demo;
 
-import java.util.logging.Logger;
-
 /**
  * @Author: sunchuanyin
  * @Date: 2019/3/14 15:28
@@ -9,7 +7,7 @@ import java.util.logging.Logger;
  */
 public class Demo {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
     }
 }
